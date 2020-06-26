696f29eb3868b380a7aabd1eceaacabe93f3133c
Hiranmay <hiranmay.cool@gmail.com>
2020-06-26 11:22:05 +0530

    'readme'

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -17,34 +17,39 @@ class Login extends React.Component {
     }
 
     handleChange = (event) => {
-        let { name, value } = event.target
-        // console.log(name, value)
-        if (name === 'userEmail') {
-            let regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-            let emailValid = value.match(regEx)
-            console.log('email valid  : ' + emailValid)
-            if (emailValid === null) {
-                this.setState({ emailError: true, emailErrorMessage: 'Enter valid email' })
-                return;
+        try {
+            let { name, value } = event.target
+            // console.log(name, value)
+            if (name === 'userEmail') {
+                let regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+                let emailValid = value.match(regEx)
+                console.log('email valid  : ' + emailValid)
+                if (emailValid === null) {
+                    this.setState({ emailError: true, emailErrorMessage: 'Enter valid email' })
+                    return;
+                }
+                else {
+                    this.setState({ emailError: false })
+                }
             }
-            else {
-                this.setState({ emailError: false })
-            }
-        }
-        if (name === 'userPassword') {
-            let regex = '(?=.*[A-Z])'
-            let pwdValid = value.match(regex)
-            console.log('pwd valid  : ' + pwdValid)
-            if (pwdValid === null) {
-                this.setState({ passwordError: true, pwdErrorMessage: 'Password must contain one uppercase letter' })
-                return;
-            }
-            else {
-                this.setState({ passwordError: false })
-            }
+            if (name === 'userPassword') {
+                let regex = '(?=.*[A-Z])'
+                let pwdValid = value.match(regex)
+                console.log('pwd valid  : ' + pwdValid)
+                if (pwdValid === null) {
+                    this.setState({ passwordError: true, pwdErrorMessage: 'Password must contain one uppercase letter' })
+                    return;
+                }
+                else {
+                    this.setState({ passwordError: false })
+                }
 
+            }
+            this.setState({ [name]: value })
         }
-        this.setState({ [name]: value })
+        catch (e) {
+            console.log(e)
+        }
     }
 
     submitLogin = () => {
